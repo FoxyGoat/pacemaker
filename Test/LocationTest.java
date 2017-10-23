cb691891b5e1ba959fdf1bac270b5c5ef074abc6
20070948 <20070948@IT101PC10.wit.ie>
2017-10-23 11:58:59 +0100

    Tests added

---
+++ Test/LocationTest.java
@@ -0,0 +1,13 @@
+package models;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class LocationTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
